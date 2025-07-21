93de9f6c3d29e6d2ad39eb60ffab7b28b37dff1c
praneethattada <praneethattada18@gmail.com>
2025-07-22 02:54:56 +0530

    made changes in css

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -4,81 +4,59 @@ import logo from '../../../images/logo.png';
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
-   
     return (
-        <footer>
+        <footer className="site-footer">
             <div className="container">
-                <div className="row">
-                    <div className="col-md-5">
-                        <img className="footer-logo" src={logo} alt="" />
-                        <p>Fusce non nulla quis leo lobortis porttitor. Quisque mi egestas nibh porttitor molestie. Nullam ut orci elit. Suspendisse tincidunt dapibu In nibh es posuere.</p>
-                        <h6>Social Link:</h6>
-                        <div className="footer-social-link">
-                            <Link to="//www.facebook.com/" target="_blank"> <i className="fab fa-facebook-f"></i></Link>
-                            <Link to="//www.twitter.com/" target="_blank"><i className="fab fa-twitter"></i></Link>
+                <div className="footer-grid">
+                    <div className="footer-brand">
+                        <img src={logo} alt="Sport&Pulse" className="footer-logo"/>
+                        <p className="footer-description">Sport&Pulse is your premier destination for sports facility bookings.</p>
+                        <div className="social-links">
+                            <Link to="#"><i className="fab fa-facebook-f"></i></Link>
+                            <Link to="#"><i className="fab fa-twitter"></i></Link>
+                            <Link to="#"><i className="fab fa-instagram"></i></Link>
                         </div>
+                    </div>
 
+                    <div className="footer-links">
+                        <h4>Quick Links</h4>
+                        <Link to="/">Home</Link>
+                        <Link to="/about">About Us</Link>
+                        <Link to="/facilities">Our Facilities</Link>
+                        <Link to="/pricing">Pricing</Link>
+                        <Link to="/contact">Contact</Link>
                     </div>
-                    <div className="col-md-2">
-                        <h5>Quick Link :</h5>
-                        <ul className="quick-link">
-                            <li>Home</li>
-                            <li>About</li>
-                            <li>Services</li>
-                            <li>Reviews</li>
-                            <li>Contact Us</li>
-                        </ul>
+
+                    <div className="footer-links">
+                        <h4>Legal</h4>
+                        <Link to="/terms">Terms of Service</Link>
+                        <Link to="/privacy">Privacy Policy</Link>
+                        <Link to="/cookies">Cookie Policy</Link>
+                        <Link to="/faq">FAQs</Link>
+                        <Link to="/support">Support</Link>
                     </div>
-                    <div className="col-md-2">
-                        <h5>Terms and Legal</h5>
-                        <ul className="quick-link">
-                            <li>Terms Of Usage</li>
-                            <li>Copyrights</li>
-                            <li>Customer Login</li>
-                            <li>Cookie Policy</li>
-                            <li>How to Contact</li>
-                        </ul>
-                    </div>
-                    <div className="col-md-3">
-                    <h5>Working Hours :</h5>
-                    <table className="table">
-                        <tbody>
-                            <tr>
-                                <th>MON</th>
-                                <td>9:00AM-8:00PM</td>
-                            </tr>
-                            <tr>
-                                <th>TUE</th>
-                                <td>9:00AM-8:00PM</td>
-                            </tr>
-                            <tr>
-                                <th>WED</th>
-                                <td>9:00AM-8:00PM</td>
-                            </tr>
-                            <tr>
-                                <th>THU</th>
-                                <td>9:00AM-8:00PM</td>
-                            </tr>
-                            <tr>
-                                <th>FRI</th>
-                                <td>9:00AM-8:00PM</td>
-                            </tr>
-                            <tr>
-                                <th>SAT</th>
-                                <td>9:00AM-8:00PM</td>
-                            </tr>
-                            <tr>
-                                <th>SUN</th>
-                                <td>9:00AM-8:00PM</td>
-                            </tr>
-                        </tbody>
-                    </table>
+
+                    <div className="footer-hours">
+                        <h4>Opening Hours</h4>
+                        <div className="hours-grid">
+                            <span>Monday</span><span>6:00 AM - 10:00 PM</span>
+                            <span>Tuesday</span><span>6:00 AM - 10:00 PM</span>
+                            <span>Wednesday</span><span>6:00 AM - 10:00 PM</span>
+                            <span>Thursday</span><span>6:00 AM - 10:00 PM</span>
+                            <span>Friday</span><span>6:00 AM - 10:00 PM</span>
+                            <span>Saturday</span><span>7:00 AM - 11:00 PM</span>
+                            <span>Sunday</span><span>7:00 AM - 9:00 PM</span>
+                        </div>
                     </div>
                 </div>
 
-            </div>
-            <div className="copyright">
-            <small>&copy; Copyright 2018, Example Corporation</small> 
+                <div className="footer-bottom">
+                    <span>&copy; {new Date().getFullYear()} Sport&Pulse. All rights reserved.</span>
+                    <div className="footer-legal-links">
+                        <Link to="/sitemap">Sitemap</Link>
+                        <Link to="/accessibility">Accessibility</Link>
+                    </div>
+                </div>
             </div>
         </footer>
     );
